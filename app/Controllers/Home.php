5e1671c270d5420f233ed2f6eac5b99ae4a1a7d1
Repan666 <alejandrorepan@gmail.com>
2025-11-14 14:09:56 +0700

    ini hasilnya

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,6 +6,11 @@ class Home extends BaseController
 {
     public function index(): string
     {
+        return view('welcome_message');
+    }
+
+    public function home(): string
+    {
         return view('Home');
     }
 }
